ec519d1026aacb0c24ed2c23e36e3fd9b4178013
astratakis <57875212+StratakisAndreas@users.noreply.github.com>
2021-03-10 12:11:08 +0200

    Created win_runner batch file and fixed input bug

---
+++ Report/win_runner.bat
@@ -0,0 +1,5 @@
+cd ..
+cd src
+set arg=%1
+java core.Console %arg%
+pause
--- src/core/Console.java
+++ src/core/Console.java
@@ -10,7 +10,15 @@ public class Console {
 	private final static String INPUT_FILE = "sampleGraph1.txt";
 	
 	public static void main(String[] args) throws FileNotFoundException {
-		Scanner scanner = new Scanner(new File("src" + File.separator + "input" + File.separator + INPUT_FILE));
+		Scanner scanner = null;
+		
+		try {
+			scanner = new Scanner(new File("input" + File.separator + args[0]));
+		}
+		catch (IndexOutOfBoundsException e) {
+			scanner = new Scanner(new File("src" + File.separator + "input" + File.separator + INPUT_FILE));
+
+		}
 		
 		ProblemInstance pi = new ProblemInstance(scanner);
 		System.out.println(pi);
