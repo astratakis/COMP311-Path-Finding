2679f5ed1bd471227697caee6ed0154269fd60e7
Dimitris Petrou <47597523+petroud@users.noreply.github.com>
2021-03-18 17:08:18 +0200

    Day class added

---
+++ src/model/Day.java
@@ -0,0 +1,15 @@
+package model;
+
+import java.util.Scanner;
+
+public class Day {
+	
+	final int ID;
+	Graph graph;	
+	
+	public Day(int ID, Graph g, Scanner sc) {
+		this.ID = ID;
+		this.graph = g;
+	}
+
+}
